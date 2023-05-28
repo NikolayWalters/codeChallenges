3721b7ab6f6377adaf0842e7263091b356a4e90b
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-05-28 15:49:00 +0100

    Added memoization with a decorator

--- fibonacci.py
+++ fibonacci.py
@@ -38,3 +38,26 @@ fibonacciMemo = memoize(fibonacciMemo)
 print(fibonacciMemo(32))  
 elapsed_time = time.time() - start_time
 print("Elapsed time: {:.2f} seconds".format(elapsed_time))
+
+# using a decorator
+
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+
+@Memoize
+def fibonacciDec(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibonacciDec(n-1)+fibonacciDec(n-2)
+print(fibonacciDec(32))
+elapsed_time = time.time() - start_time
+print("Elapsed time: {:.2f} seconds".format(elapsed_time))
