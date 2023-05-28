56b6bfd8fb35721f78472503613a08c5227c840e
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-05-28 14:49:07 +0100

    Added a function to gen Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+"""Compute the nth Fibonacci number"""
+
+def fibonacci(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(5))
