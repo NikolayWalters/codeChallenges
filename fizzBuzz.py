ceeabba7ffe8433540cc84e49d02e140bfe5c262
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-06-25 20:07:49 +0100

    fizzBuzz script with a list comprehension

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -6,12 +6,21 @@ multiples of both 3 and 5, print "FizzBuzz".
 
 # multiples of 3 and 5 -> 15
 
-for i in range (1,101):
-	if i % 15 == 0:
-		print('FizzBuzz')
-	elif i % 5 == 0:
-		print('Buzz')
-	elif i % 3 == 0:
-		print('Fizz')
-	else:
-		print(i)
+def fizz():
+	for i in range (1,101):
+		if i % 15 == 0:
+			print('FizzBuzz')
+		elif i % 5 == 0:
+			print('Buzz')
+		elif i % 3 == 0:
+			print('Fizz')
+		else:
+			print(i)
+
+# or using a list comprehension
+def fizzN(N):
+	result = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i for i in range(1, N+1)]
+	for item in result:
+		print(item)
+
+fizzN(20)
